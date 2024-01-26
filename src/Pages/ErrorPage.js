d2667f931c8ffdcfe5547231238d5506afc0f3ce
GD0122 <galuhdental55@gmail.com>
2024-01-27 05:51:05 +0700

    add lagi

--- src/Pages/ErrorPage.js
+++ src/Pages/ErrorPage.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function ErrorPage() {
+    useEffect(()=>{
+        setInterval(()=>{
+            Nav('/')
+        },5000)
+    })
   return (
     <div>
         <div style={{minHeight:'1000px',display:'flex',alignItems:'center'}}>
