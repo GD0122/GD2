04fce3023aa8d234dfb00fd9605b1772d5d343f4
GD0122 <galuhdental55@gmail.com>
2024-02-24 16:34:56 +0700

    test

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -2,26 +2,19 @@ const axios = require('axios')
 require("dotenv").config()
 
 exports.handler = async(event,context)=>{
-    const salt = "HJSHdjajshjdhasyduyshJHJUSyui187suiU7s6d678"
-   
+  
     try {
        let response = await axios.get(`https://script.google.com/macros/s/${process.env.REACT_APP_KEY}/exec`,{
         Headers:{Accept:'application/json',"Accept-Encoding":"identity"},
         params:{trophies:true}
        })
-       let datas = response.data
-    //    const o = JSON.stringify(datas).split('');
-    //    for(var i = 0, l = o.length; i < l; i++)
-    //      if(o[i] == '{')
-    //          o[i] = '}';
-    //      else if(o[i] == '}')
-    //          o[i] = '{';
-    //     const data= encodeURI(salt + o.join(''));
       
+
+       let data = response
       
        return{
         statusCode:200,
-        body: JSON.stringify({datas})
+        body: JSON.stringify({data})
        }
     } catch (error) {
         return{
